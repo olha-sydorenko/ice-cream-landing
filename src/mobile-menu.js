8354dcad5ddaba31953c9aafa11a158dceca6447
Tetiana Novitska <novittana2410@gmail.com>
2022-11-26 16:20:38 +0200

    header-hero-css

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -4,13 +4,20 @@
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
     body: document.querySelector("body"),
+    menuList: document.querySelector('.mob-header-menu'),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.menuList.addEventListener('click', removeMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
+    refs.menu.classList.toggle("mob-menu-hidden");
     refs.body.classList.toggle("no-scroll");
+  }
+
+  function removeMenu() {
+    refs.menu.classList.add('mob-menu-hidden');
+    refs.body.classList.remove('no-scroll');
   }
 })();
