fe00fe61aa9112f92bc375db1bb7a5190dd9708a
Tetiana Novitska <novittana2410@gmail.com>
2022-11-27 17:09:19 +0200

    animation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import './js/mobile-menu';
 import './js/swiper';
 import './js/swipertwo';
 import './js/franchise';
 import './js/modal-buy-now';
 import './js/modal-product-info';
-import './js/mobile-menu';
+
 
